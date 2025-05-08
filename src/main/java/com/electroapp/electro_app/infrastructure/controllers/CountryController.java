ff392e2935f8503b205879f05aea591868d02ae0
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2025-05-08 15:12:24 -0500

    feat: :sparkles: Add new feature
    Add new feature

--- src/main/java/com/electroapp/electro_app/infrastructure/controllers/CountryController.java
+++ src/main/java/com/electroapp/electro_app/infrastructure/controllers/CountryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,15 @@ public class CountryController {
     @PostMapping
     public ResponseEntity<?> create(@RequestBody Country country) {
         return ResponseEntity.status(HttpStatus.CREATED).body(countryService.save(country));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Country country, @PathVariable Long id) {
+        Optional<Country> countryOptional = countryService.update(id, country);
+        if (countryOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(countryOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
